60839432cb14d4b5850b0e8932ec027e87c8e071
Suraj Sharma <125715507+surajsharma111@users.noreply.github.com>
2024-08-01 14:37:58 +1000

    fixed some errrors

--- frontend/src/action/deleteTodo.js
+++
@@ -1,9 +0,0 @@
-
-import { API_ENDPOINT } from "../constant/constant";
-
- async function deleteTodo(){
-   const response = await fetch(`${API_ENDPOINT}/`)
-   const data =  await response.json();
-   return data;
-}
-export default deleteTodo
--- frontend/src/action/getTodo.js
+++ frontend/src/action/getTodo.js
@@ -1,6 +1,8 @@
-
 import { API_ENDPOINT } from "../constant/constant";
 
- async function deleteTodo(id){
- }
- export default deleteTodo
+ async function getAllTodos(){
+   const response = await fetch(`${API_ENDPOINT}/todos`)
+   const data =  await response.json();
+   return data;
+}
+export default getAllTodos
