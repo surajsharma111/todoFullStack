6ae7872d23f9d3533f90def2160042ece0311df4
Suraj Sharma <125715507+surajsharma111@users.noreply.github.com>
2024-08-07 19:25:44 +1000

    removed jade view

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'ToDo' });
+  res.status(200).send('To Do API');
 });
 
 export default router
